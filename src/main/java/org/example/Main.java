21c5b990794d99420f118393b15dd835a8a02d9e
Cabavas <cabavas@gmail.com>
2023-05-21 08:40:01 -0300

    "Finished"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.example.entities.Account;
+import org.example.exceptions.BusinessException;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number;
+        String holder;
+        double initialBalance, withdrawLimit, amount;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.next();
+        holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        withdrawLimit = sc.nextDouble();
+        System.out.println();
+        Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+        System.out.print("Enter amount for withdraw: ");
+        amount = sc.nextDouble();
+        try {
+            acc.withdraw(amount);
+            System.out.printf("New balance: %.2f", acc.getBalance());
+        } catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+        sc.close();
+
     }
 }
---
+++ src/main/java/org/example/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class BusinessException extends Exception {
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
